c150cfbdeb5ce7e9ce6684e797e1eb66f5541a02
s-013-27 <s-013-27@gumiforest.ae.krakow.pl>
2023-10-27 18:05:46 +0200

    add new afterclass files

---
+++ 02-ClassesAndObjects/SpeedingMain.java
@@ -0,0 +1,11 @@
+public class SpeedingMain {
+    public static void main(String[] args) {
+        int speed = 5;
+
+        if (speed >= 40 & speed <= 140)
+        {
+            System.out.println("True");
+        }
+        else {System.out.println("False");}
+    }
+}
